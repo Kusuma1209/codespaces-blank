3d34dece0258ddefbc8dcd10397aea970dc41182
Kusuma Katari <116937659+Kusuma1209@users.noreply.github.com>
2026-04-14 06:42:18 +0000

    commit changes

--- tests/apiTesting.spec.ts
+++ tests/apiTesting.spec.ts
@@ -7,8 +7,26 @@ test.only('GET API', async ({request}, testInfo)=>{
     expect(response.status()).toBe(200);
     // console.log(response);
 
-    testInfo.attach('response', {
-        body: JSON.stringify(await response.json(), null, 2),
+    const responseBody = await response.json();
+    expect(responseBody.length).toBe(100);
+    expect(responseBody[0]).toHaveProperty('id');
+    expect(responseBody[0]).toHaveProperty('title');
+    expect(responseBody[0]).toHaveProperty('body');
+    expect(responseBody[3].id).toBe(4);
+    expect(responseBody[1].title).toBe('qui est esse');
+
+    testInfo.attach('GET Response', {
+        body: JSON.stringify(responseBody, null, 2),
         contentType: 'application/json'
     });
+    //console.log(responseBody);
+
 });
+
+test('POST API', async ({request}, testInfo)=>{
+    const postData = {
+        title: 'foo',
+        body: 'bar',
+        userId: 1
+    };
+);
