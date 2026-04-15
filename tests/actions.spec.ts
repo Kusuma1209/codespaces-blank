d8636f13658e5423a72261f5ad6627b218c00476
Kusuma Katari <116937659+Kusuma1209@users.noreply.github.com>
2026-04-15 13:24:13 +0000

    commit changes

--- tests/actions.spec.ts
+++ tests/actions.spec.ts
@@ -2,5 +2,18 @@ import { test, expect } from '@playwright/test';
 
 test('practice actions', async ({ page }) => {
     await page.goto('https://playwright.dev/');
+
     await page.getByRole('link', { name: 'Get started' }).click();
-    
+
+    await page.getByRole('heading', { name: 'Introduction' }).hover();
+
+    await page.getByText('Search').click();
+
+    const searchBox = page.getByPlaceholder('Search docs');
+
+    await expect(searchBox).toBeVisible(); // key fix
+
+    await searchBox.fill('locators');
+
+    await searchBox.press('Enter'); // better than keyboard
+});
