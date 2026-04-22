b1f10ef848ffb0465c4c7a396bfe9041a279ea48
Kusuma Katari <116937659+Kusuma1209@users.noreply.github.com>
2026-04-22 11:24:30 +0000

    commit changes

--- tests/actions.spec.ts
+++ tests/actions.spec.ts
@@ -13,7 +13,21 @@ test('practice actions', async ({ page }) => {
 
     await expect(searchBox).toBeVisible(); // key fix
 
-    await searchBox.fill('locators');
+    //await searchBox.fill('locators');
+    await searchBox.type('locators');
+
+    await page.screenshot({path:'screenshot3.png'});
 
     await searchBox.press('Enter'); // better than keyboard
+
+    await expect(page).toHaveURL('https://playwright.dev/docs/locators');
+    await page.screenshot({path:'screenshot4.png'});
+    
+    await page.getByRole('link',{name:'page.getByTestId()'}).first().isVisible();
+    
+    await page.getByRole('link',{name:'page.getByTestId()'}).nth(0).click();
+    await page.getByRole('heading',{name:'Locate by test id'}).isVisible();
+    await page.getByRole('heading',{name:'Locate by test id'}).highlight();
+    //await page.getByRole('heading',{name:'Locate by test id'}).hover();
+    await page.screenshot({path:'screenshot5.png'});
 });
