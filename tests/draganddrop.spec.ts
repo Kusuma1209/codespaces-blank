b6287de3b7c28d85339c35dceaebb773c6b35687
Kusuma Katari <116937659+Kusuma1209@users.noreply.github.com>
2026-04-29 08:21:24 +0000

    commit changes

---
+++ tests/draganddrop.spec.ts
@@ -0,0 +1,15 @@
+import test, { expect } from "@playwright/test";
+
+test('draganddrop', async({page})=>{
+    await page.goto('https://the-internet.herokuapp.com/drag_and_drop');
+
+    const colA= page.locator('#column-a');
+    const colB=page.locator('#column-b');
+
+    await colA.dragTo(colB);
+
+    await expect(colA).toHaveText('B');
+    await expect(colB).toHaveText('A');
+
+    
+});
--- tests/hovereffect.spec.ts
+++ tests/hovereffect.spec.ts
@@ -1,8 +1,22 @@
 import{ test, expect} from '@playwright/test';
-test('hover',async({page})=>{
+test('hover', async({page})=>{
     await page.goto('https://the-internet.herokuapp.com/hovers');
-    const locator=page.locator('.figure');
-    const onhover=page.locator('.figcaption');
+    const figures=page.locator('.figure');
 
-    await locator.nth(0).hover();
+    //for(let i=0; i<3;i++){
+        const figure=figures.nth(0);
+        await figure.hover();
+        const caption= figure.locator('.figcaption');
+
+        await expect(caption).toBeVisible();
+        await expect(caption.locator('h5')).toHaveText('name: user1');
+        await expect(caption.locator('a')).toBeVisible();
+        await page.screenshot({path:'hover1.png'});
+        await page.getByRole('link',{name:'View profile'}).first().click();
+        
+
+        await page.getByRole('heading',{name:'Not Found'}).isVisible();
+        await page.screenshot({path:'hover2ss.png'});
+    //}
+
 });
